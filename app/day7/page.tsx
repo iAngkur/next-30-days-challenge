7d9a47412803edecad0757b9acbd016eac9c9165
Mon <mon.messaging@gmail.com>
2025-03-27 15:01:26 +0600

    fixed alignment

--- app/day7/page.tsx
+++ app/day7/page.tsx
@@ -1,4 +1,7 @@
-import { Button, List, ListItem, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 
